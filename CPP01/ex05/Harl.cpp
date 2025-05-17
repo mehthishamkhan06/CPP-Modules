63502a211cbb974bdb56a4448143bfab79d7f65e
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-05-17 18:49:27 +0400

    almost done

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -9,25 +9,25 @@ Harl::~Harl()
 
 }
 
-void Harl::debug(void)
+void Harl::_debug(void)
 {
     std::cout << "I love having extra beef for my 7XL-double-cheese-triple" <<
 				"-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
-void	Harl::info(void)
+void	Harl::_info(void)
 {
 	std::cout << "I cannot believe adding extra beef costs more money. You" <<
 				" didn't put enough beef in my burger! If you did, I " <<
 				"wouldn't be asking for more!" << std::endl;
 }
-void	Harl::warning(void)
+void	Harl::_warning(void)
 {
 	std::cout << "I think I deserve to have some extra beef for free. I've" <<
 				" been coming for years whereas you started working here " <<
 				"since last month." << std::endl;
 }
-void	Harl::error(void)
+void	Harl::_error(void)
 {
 	std::cout << "This is unacceptable! I want to speak to the manager now." <<
 					std::endl;
@@ -36,10 +36,10 @@ void	Harl::error(void)
 void Harl::complain(std::string level)
 {
     void (Harl::*levelassign[4])();
-    levelassign[0] = &Harl::debug;
-    levelassign[1] = &Harl::info;
-    levelassign[2] = &Harl::warning;
-    levelassign[3] = &Harl::error;
+    levelassign[0] = &Harl::_debug;
+    levelassign[1] = &Harl::_info;
+    levelassign[2] = &Harl::_warning;
+    levelassign[3] = &Harl::_error;
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     for (int i = 0; i < 4; i++)
     {
--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -10,10 +10,10 @@ class Harl{
         ~Harl();
         void complain( std::string level );
     private:
-        void debug(void);
-        void info(void);
-        void warning(void);
-        void error(void);
+        void _debug(void);
+        void _info(void);
+        void _warning(void);
+        void _error(void);
 };
 
 #endif
