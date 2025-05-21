88bc6c83fc3d43e0ff09c58bdbdd8ed421028e30
Mohammed Ehthisham Ullah Khan <mohkhan@lab3r7s9.42abudhabi.ae>
2025-05-21 19:06:00 +0400

    cpp02 ex01 done

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -8,11 +8,13 @@ Fixed::Fixed(): _fixed(0)
 Fixed::Fixed(const int a)
 {
     std::cout << "Int constructor called" << std::endl;
+    this->_fixed = a << this->_bits; // convert integer to fixed-point representation
 }
 
 Fixed::Fixed(const float a)
 {
     std::cout << "Float constructor called" << std::endl;
+    this->_fixed = roundf(a * (1 << this->_bits)); // convert float to fixed-point representation
 }
 
 Fixed::Fixed(const Fixed &object)
@@ -32,4 +34,31 @@ Fixed& Fixed::operator=(Fixed const &right_value)
     if (this != &right_value) // checks for self assigning
         this->_fixed = right_value.getRawBits(); // if its not same varible like a=a then it copies the rawbits to _fixed.
     return (*this);  // returns all instances
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f) {
+    os << f.toFloat();  // output float representation of fixed-point value
+    return os;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_fixed);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixed = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+    return ((float)this->_fixed / (1 << this->_bits)); // convert fixed-point representation to float
+}
+
+int Fixed::toInt( void ) const
+{
+    return (this->_fixed >> this->_bits); // convert fixed-point representation to integer
 }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,10 +6,28 @@
 
 class Fixed{
     private:
-        int _fixed_pointer;
+        int _fixed;
         static const int _bits = 8;
 
     public:
+        //Contructors
+        Fixed();
+        Fixed(const int a);
+        Fixed(const float a);
+        //Copy constructor
+        Fixed(const Fixed &object);
+        //Destructor
+        ~Fixed();
+        //Copy assignment operator
+        Fixed& operator=(Fixed const &right_value);
         float toFloat( void ) const;
         int toInt( void ) const;
-}
+
+        //from previous exrcise
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
+
+#endif
