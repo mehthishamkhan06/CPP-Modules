6c00baada1960e31f32dd1fb9ae2087d33ac26af
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-08-04 12:41:05 +0400

    CPP05/ex00

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <limits>
+
+class Bureaucrat {
+    private:
+        std::string _name;
+        int _grade;
+
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        Bureaucrat &operator=(const Bureaucrat &copy);
+        ~Bureaucrat();
+        void gradeDecrement();
+        void gradeIncrement();
+        std::string getName() const;
+        int getGrade() const;
+        
+};
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &copy);
+
+#endif
