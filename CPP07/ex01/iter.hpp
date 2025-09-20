27d5e3747d5dc3367ec5a353d9bf833631f3e674
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-09-20 15:30:25 +0400

    ex01 done Alhamdulillah

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,18 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+template <typename T>
+void	iter(T *array, size_t length, void(*f)(T const &))
+{
+	for (size_t i=0; i < length; i++){
+		f(array[i]);
+	}
+}
+
+template <typename T>
+void	print(T const &x)
+{
+	std::cout << x << std::endl;
+}
+#endif
---
+++ CPP07/ex01/main.cpp
@@ -0,0 +1,8 @@
+#include "iter.hpp"
+
+
+int main()
+{
+    int array[10] = {0,1,2,3,4,5,6,7,8,9};
+    iter(array, 10, print);
+}
