465a27a6d141008046171cdb16239efc6def02cd
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-09-22 00:05:08 +0400

    need to add one more function

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -1,27 +1,40 @@
 #include "Span.hpp"
 #include <iostream>
 
-int main() {
-    try {
-        Span sp(3);  // capacity is 3
+// int main() {
+//     try {
+//         Span sp(3);  // capacity is 3
 
-        sp.addNumber(10);
-        sp.addNumber(25);
-        sp.addNumber(30);
-        int longest = sp.longestSpan();
-        int shortest = sp.shortestSpan();\
-        std::cout << longest <<std::endl;
-        std::cout << shortest <<std::endl;
+//         sp.addNumber(10);
+//         sp.addNumber(25);
+//         sp.addNumber(30);
+//         int longest = sp.longestSpan();
+//         int shortest = sp.shortestSpan();
+//         std::cout << longest <<std::endl;
+//         std::cout << shortest <<std::endl;
         
-        std::cout << "All numbers added successfully!" << std::endl;
+//         std::cout << "All numbers added successfully!" << std::endl;
 
-        //Error
-        sp.addNumber(40);
+//         //Error
+//         sp.addNumber(40);
 
-    }
-    catch (std::exception &e) {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
+//     }
+//     catch (std::exception &e) {
+//         std::cout << "Exception caught: " << e.what() << std::endl;
+//     }
 
+//     return 0;
+// }
+
+int main()
+{
+    Span sp = Span(5);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
     return 0;
 }
