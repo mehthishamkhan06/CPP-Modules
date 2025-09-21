1c5f2d00696027fb5fd7c662b970a4d3be133da6
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2025-09-21 21:57:59 +0400

    updated main

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -8,9 +8,23 @@ int main()
     v.push_back(3);
     v.push_back(4);
     v.push_back(5);
+    std::list<int> l;
+    l.push_back(5);
+    l.push_back(6);
+    l.push_back(7);
+    l.push_back(8);
+    l.push_back(9);
+
     try {
         std::cout << "Found: " << *easyfind(v, 3) << std::endl;
         std::cout << "Not Found: " << *easyfind(v, 8) << std::endl;
+    }
+    catch (std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        std::cout << "Found: " << *easyfind(l, 8) << std::endl;
+        std::cout << "Not Found: " << *easyfind(l, 19) << std::endl;
     }
     catch (std::exception &e){
         std::cout << e.what() << std::endl;
